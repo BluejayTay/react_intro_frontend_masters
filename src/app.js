ac5fb0717412b6fe5b9ea2b14030fc7f76f281aa
Taylor Rusthoven <92177706+BluejayTay@users.noreply.github.com>
2022-02-16 16:06:26 -0600

    include eslint

--- src/app.js
+++ src/app.js
@@ -7,16 +7,24 @@ const Pet = (props) => {
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-      React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havanese"}),
-      React.createElement(Pet, {name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-      React.createElement(Pet, {name: "Sudo", animal: "Dog", breed: "Wheaten Terrier"}),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
